9f4725fa331e1947c0b18f850aebfe8bad4e749d
Carolina Freire <carolinacervelaf@gmail.com>
2022-01-11 21:20:10 -0300

    subindo o projeto pronto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './assets/css/base/base.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
+import Pagina404 from './paginas/Pagina404'
+import Cabecalho from './components/Cabecalho'
+import Post from './paginas/Post'
+import Categoria from './paginas/Categoria'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Cabecalho />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/sobre'>
+          <Sobre />
+        </Route>
+        <Route path='/categoria/:id'>
+          <Categoria />
+        </Route>
+        <Route path='/posts/:id'>
+          <Post />
+        </Route>
+        <Route>
+          <Pagina404 />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
